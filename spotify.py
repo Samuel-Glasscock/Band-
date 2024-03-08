43e9b59a7aa2eae7fd20304f8cf096d0359e5500
Samuel Glasscock <samuel.c.glasscock@gmail.com>
2024-03-08 01:31:34 -0500

    edited debugging lines and added docstrs

--- spotify.py
+++ spotify.py
@@ -11,7 +11,7 @@ SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 SPOTIFY_CACHE_PATH = os.getenv("SPOTIFY_CACHE_PATH")
 PLAYLIST_ID = os.getenv("PLAYLIST_ID")
 
-# config app info
+# Configure app info
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     client_id=SPOTIFY_CLIENT_ID,
     client_secret=SPOTIFY_CLIENT_SECRET,
@@ -22,6 +22,15 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
 
 
 def get_playlist_tracks(playlist_id):
+    """
+    Retrieve tracks from a Spotify playlist
+
+    Args:
+        playlist_id (string): hashcode retrieved from playlist when you go to the '...' and then 'share' on a specific playlist
+
+    Returns:
+        list: list of tuples containing track name and artist name
+    """
     tracks_data = []
     results = sp.playlist_tracks(playlist_id)
     tracks = results['items']
@@ -40,7 +49,8 @@ def get_playlist_tracks(playlist_id):
 
     return tracks_data
 
+# For testing your how your playlist data displaying
 
-playlist_tracks = get_playlist_tracks(PLAYLIST_ID)
-for track_name, artist_name in playlist_tracks:
-    print(f"{track_name} by {artist_name}")
+# playlist_tracks = get_playlist_tracks(PLAYLIST_ID)
+# for track_name, artist_name in playlist_tracks:
+#     print(f"{track_name} by {artist_name}")
