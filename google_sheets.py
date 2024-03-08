06cbe9dbe9d96414997306d9b1441885fedfbc81
Samuel Glasscock <samuel.c.glasscock@gmail.com>
2024-03-08 00:54:54 -0500

    implemented gspread functions to populate spreadsheet

--- google_sheets.py
+++ google_sheets.py
@@ -4,8 +4,21 @@ import gspread
 
 load_dotenv()
 
-SERVICE_ACCOUNT_FILE = os.getenv('SERVICE_ACCOUNT_PATH')
 
-# Authenticate bot with GoogleSsheets API
-client = gspread.service_account(filename=SERVICE_ACCOUNT_FILE)
+def setup_google_sheet(sheet_title):
+    SERVICE_ACCOUNT_FILE = os.getenv('SERVICE_ACCOUNT_PATH')
 
+    # Authenticate bot with GoogleSsheets API
+    client = gspread.service_account(filename=SERVICE_ACCOUNT_FILE)
+    
+    worksheet1 = client.open(sheet_title).sheet1
+
+    return worksheet1
+
+
+def batch_update_sheet(sheet, data, start_row, start_col):
+    end_row = start_row + len(data) - 1
+    end_col = chr(ord(start_col) + 1)
+    range = f"{start_col}{start_row}:{end_col}{end_row}"
+
+    sheet.update(range, data)
